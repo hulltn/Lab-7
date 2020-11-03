eb1183b7d7ee22f5ebefd91f1e606ffefb6ffda6
hulltn <hulltn@mail.uc.edu>
2020-11-03 07:06:48 +0000

    Initial commit

---
+++ Counter.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Counter implements 
+ActionListener {
+  JLabel leftLabel;
+  JLabel rightLabel;
+  int leftCount;
+  int rightCount;
+
+  Counter() {
+    
+  }
+}
